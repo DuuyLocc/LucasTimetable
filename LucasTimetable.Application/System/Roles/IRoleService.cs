fc8ddd9925b865ce193060ec4689638c2f2e75fd
Lucas Le <duuylocc@outlook.com.vn>
2020-11-17 11:41:19 +0700

    done role

---
+++ LucasTimetable.Application/System/Roles/IRoleService.cs
@@ -0,0 +1,13 @@
+﻿using LucasTimetable.ViewModel.System.Roles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTimetable.Application.System.Roles
+{
+    public interface IRoleService
+    {
+        Task<List<RoleViewModel>> GetAll();
+    }
+}
